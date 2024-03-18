3224f962241f7d0a28161410627609dc343cfe9b
Daniel Morales <155024431+speedbump5421@users.noreply.github.com>
2024-03-18 12:03:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+
+// let myAge = 40
+// let humanDogRatio = 7
+
+// let count = myAge * humanDogRatio
+
+// console.log(count)
+
+// let count = 50
+// count = count + 50
+// console.log(count)
+
+// count = count - 75
+// console.log(count)
+
+// count = count + 45
+// console.log(count)
+
+function increment() {
+    console.log("The button was clicked")
+}
